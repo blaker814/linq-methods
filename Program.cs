1c230dcddb675c5f5718082c7d61edc4d2f458a6
blaker814 <rab0196@yahoo.com>
2020-11-19 13:14:18 -0600

    finished custom types

--- Program.cs
+++ Program.cs
@@ -96,15 +96,79 @@ namespace linq
 
                 Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
             */
-            List<int> wheresSquaredo = new List<int>()
-            {
-                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            // List<int> wheresSquaredo = new List<int>()
+            // {
+            //     66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            // };
+            // List<int> untilSquare = wheresSquaredo.TakeWhile(num => !(Math.Sqrt(num) % 1 == 0)).ToList();
+            // foreach (int num in untilSquare)
+            // {
+            //     Console.WriteLine(num);
+            // }
+
+
+            // Build a collection of customers who are millionaires
+            // List<Customer> customers = new List<Customer>() {
+            //     new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            //     new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            //     new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            //     new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            //     new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            //     new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            //     new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            //     new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            //     new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            //     new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            // };
+            // List<Customer> millionaires = customers.Where(customer => customer.Balance >= 1000000).ToList();
+            // foreach (Customer customer in millionaires)
+            // {
+            //     Console.WriteLine(customer.Name);
+            // }
+
+
+            /*
+                Given the same customer set, display how many millionaires per bank.
+                Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+            */
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
             };
-            List<int> untilSquare = wheresSquaredo.TakeWhile(num => !(Math.Sqrt(num) % 1 == 0)).ToList();
-            foreach (int num in untilSquare)
+            IEnumerable<Bank> millionairesBanks = from customer in customers
+                                                  where customer.Balance >= 1000000
+                                                  group customer by customer.Bank into memberBanks
+                                                  select new Bank { Name = memberBanks.Key, TotalMembers = memberBanks.Count() };
+            foreach (Bank bank in millionairesBanks)
             {
-                Console.WriteLine(num);
+                Console.WriteLine($"{bank.Name} {bank.TotalMembers}");
             }
         }
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
+        public class Bank
+        {
+            public string Name { get; set; }
+            public int TotalMembers { get; set; }
+        }
+
     }
 }
