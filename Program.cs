73b555882cf37a0624f5578365047592f49a7826
blaker814 <rab0196@yahoo.com>
2020-11-19 11:37:54 -0600

    finished ordering

--- Program.cs
+++ Program.cs
@@ -8,23 +8,54 @@ namespace linq
     {
         static void Main(string[] args)
         {
-            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
-            IEnumerable<string> LFruits = from fruit in fruits
-                                          where fruit.StartsWith("L")
-                                          select fruit;
+            // Find the words in the collection that start with the letter 'L'
+            // List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+            // IEnumerable<string> LFruits = from fruit in fruits
+            //                               where fruit.StartsWith("L")
+            //                               select fruit;
             // foreach (string fruit in LFruits)
             // {
             //     Console.WriteLine(fruit);
             // }
 
+
+            // Which of the following numbers are multiples of 4 or 6
+            // List<int> numbers = new List<int>()
+            // {
+            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            // };
+            // IEnumerable<int> fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 == 0);
+            // foreach (int num in fourSixMultiples)
+            // {
+            //     Console.WriteLine(num);
+            // }
+
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            // List<string> names = new List<string>()
+            // {
+            //     "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+            //     "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+            //     "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+            //     "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+            //     "Francisco", "Tre"
+            // };
+            // List<string> descend = names.OrderByDescending(name => name.ToLower()).ToList();
+            // foreach (string name in descend)
+            // {
+            //     Console.WriteLine(name);
+            // }
+
+
+            // Build a collection of these numbers sorted in ascending order
             List<int> numbers = new List<int>()
             {
                 15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
             };
-
-
-            IEnumerable<int> fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 == 0);
-            foreach (int num in fourSixMultiples)
+            IEnumerable<int> ascend = from num in numbers
+                                      orderby num ascending
+                                      select num;
+            foreach (int num in ascend)
             {
                 Console.WriteLine(num);
             }
